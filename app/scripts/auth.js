9f3856f7639c7bce4f6d8e158d4e51e21a5a867b
theentirety <amartlage@gmail.com>
2014-02-23 18:17:04 -0800

    Auth is now working. Added proximal.

--- app/scripts/auth.js
+++ app/scripts/auth.js
@@ -12,10 +12,113 @@ function Auth(app) {
 
 	var auth = app.myViewModel.auth = {};
 
-	auth.test = ko.observable('what up auth');
+	auth.currentUser = ko.observable();
+	auth.errorMessage = ko.observable('');
+	auth.signUpMode = ko.observable(false);
 
-	auth.testFunction = function() {
-		auth.test('new text')
+	var currentUser = Parse.User.current();
+	if (currentUser) {
+		auth.currentUser(currentUser);
+	}
+
+	auth.resetError = function() {
+		auth.errorMessage('');
+	}
+
+	auth.signInUp = function(formElement) {
+		// ko.postbox.publish('isLoading', true);
+		auth.resetError();
+
+		var username = $(formElement).find('input[name=auth_username]').val();
+		var password = $(formElement).find('input[name=auth_password]').val();
+
+		if (auth.signUpMode()) {
+			var email = $(formElement).find('input[name=auth_email]').val();
+			var displayName = $(formElement).find('input[name=auth_displayName]').val();
+			var passwordConfirm = $(formElement).find('input[name=auth_confirmPassword]').val();
+
+			// validation
+			if (email.length < 1) {
+				auth.errorMessage('Please enter your email address.');
+				return false;
+			}
+
+			if (displayName.length < 1) {
+				auth.errorMessage('Please enter your first and last name.');
+				return false;
+			}
+
+			if (username.length < 1) {
+				auth.errorMessage('Please enter a username.');
+				return false;
+			}
+
+			if (password.length < 1 || passwordConfirm < 1 || password != passwordConfirm) {
+				auth.errorMessage('Please enter and confirm a password.');
+				return false;
+			}
+
+			var user = new Parse.User();
+			var scrubbedUsername = username.replace(/\s+/g,""); //remove white space
+			scrubbedUsername = scrubbedUsername.toLowerCase();
+
+			user.set('username', scrubbedUsername);
+			user.set('password', password);
+			user.set('email', email);
+			user.set('displayName', displayName);
+
+			// other fields can be set just like with Parse.Object
+			// user.set("phone", "415-392-0202");
+
+			user.signUp(null, {
+				success: function(user) {
+					auth.currentUser(user);
+				},
+				error: function(user, error) {
+					auth.errorMessage(auth.sanitizeErrors(error));
+					console.log(error);
+				}
+			});
+
+		} else {
+			Parse.User.logIn(username, password, {
+				success: function(user) {
+					auth.currentUser(user);
+				},
+				error: function(user, error) {
+					// The login failed. Check error to see why.
+					auth.errorMessage(auth.sanitizeErrors(error));
+					console.log(error);
+				}
+			});
+		}
+	}
+
+	auth.logout = function() {
+		Parse.User.logOut();
+		auth.currentUser(null);
+	}
+
+	auth.showSignUp = function() {
+		console.log('asdf')
+		auth.errorMessage('');
+		if (auth.signUpMode()) {
+			auth.signUpMode(false);
+		} else {
+			auth.signUpMode(true);
+		}
+	}
+
+	auth.sanitizeErrors = function(error) {
+		switch(error.code)
+		{
+			case 101:
+				return 'Please enter a valid full name and password.';
+			case 124:
+				return 'Oops! We messed up. Please try again.';
+			default:
+				return error.message.charAt(0).toUpperCase() + error.message.slice(1) + '.';
+		}
 	}
 
 	return self;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,6 +10,12 @@
 var App = require('./app.js');
 var Auth = require('./auth.js');
 
+// initialize parse
+Parse.initialize("JkYNfPBw2aPgcc7PeTGHMAU2XKvjzeqVIkyClVuo", "45OMU3ZS3o5c168lQxa0ilxQu4FdMVHT1NVTkORl");
+
+// initialize typekit
+try{Typekit.load();}catch(e){}
+
 var app = new App();
 var auth = new Auth(app);
 
